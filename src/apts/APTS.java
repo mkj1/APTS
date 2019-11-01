1d53116d58c842b6e62a373868231ebbd2b4a607
Mads Kring <mads00@gmail.com>
2019-11-01 17:16:06 +0100

    added XChart

--- src/apts/APTS.java
+++ src/apts/APTS.java
@@ -6,6 +6,8 @@
 package apts;
 
 import java.util.LinkedList;
+import org.knowm.xchart.*;
+
 
 /**
  *
@@ -23,28 +25,49 @@ public class APTS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        final int duration = 300;               
         Initialize();
-    
-        for(int i = 0; i < 200; i++){
+        
+        // Arrays for chart
+        double[] xData = new double[duration];
+        double[] yData = new double[duration];
+        
+        for(int i = 0; i < duration; i++){
             Update();
+            xData[i] = i;
+            yData[i] = passengerQueue.size();
         }
         
+
+
+        // Create Chart
+        XYChart chart = QuickChart.getChart("APTS", "minutes", "queue length", "y(x)", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();    
     }
     
     
     public static void Initialize(){
-        gate1 = new Gate(1);
+        gate1 = new Gate(12);
         passengerQueue = new LinkedList<Passenger>();
         vehicleQueue = new LinkedList<Car>();
         vehicleQueue.add(new Car("A", 10));
         vehicleQueue.add(new Car("B", 10));
+        vehicleQueue.add(new Car("C", 10));
+        vehicleQueue.add(new Car("D", 10));
+        vehicleQueue.add(new Car("E", 10));
+        vehicleQueue.add(new Car("F", 10));
+        
+        for(int i = 0; i<10; i++){
+            passengerQueue.add(new Passenger("P" + tick + i, gate1,tick));
+        }
+        
     }
     
     public static void Update(){
-        if(tick%5==0){
+        if(tick%4==0){
            passengerQueue.add(new Passenger("P" + tick, gate1,tick));
-           passengerQueue.add(new Passenger("A" + tick, gate1,tick)); 
         }
         
         if(!vehicleQueue.isEmpty()){
